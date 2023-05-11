02f88ef07bf5f007cfb6f9f5ae97a7012cdbc6f9
Akshay Chauhan <akshuchauhan301@gmail.com>
2023-05-11 23:06:55 +0530

    links update,redundant content removal

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -171,24 +171,7 @@ const Services = () => {
       </div>
     </div>
   </section>
-  <section
-    className="ftco-section ftco-intro"
-    style={{ backgroundImage: "url(images/bg_3.jpg)" }}
-  >
-    <div className="overlay" />
-    <div className="container">
-      <div className="row justify-content-end">
-        <div className="col-md-6 heading-section heading-section-white ftco-animate">
-          <h2 className="mb-3">
-            Do You Want To Earn With Us? So Don't Be Late.
-          </h2>
-          <a href="#" className="btn btn-primary btn-lg">
-            Become A Driver
-          </a>
-        </div>
-      </div>
-    </div>
-  </section>
+  
   <footer className="ftco-footer ftco-bg-dark ftco-section">
     <div className="container">
       <div className="row mb-5">
